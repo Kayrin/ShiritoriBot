f91af9026175b37958a0154886cedaa2a47f7240
Huc <hvu295@gmail.com>
2017-10-12 18:50:35 +0200

    Comments

--- main.js
+++ main.js
@@ -19,16 +19,24 @@ client.on('message', message => {
   // command = say
   // args = ["Is", "this", "the", "real", "life?"]
   const args = message.content.slice(settings.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
+  const command = args.shift().toLowerCase(); // '>' is the current prefix, set in settings.json
 
 
+  // ping command, shows the latency
   if (message.content === 'ping') {
-    message.reply('pong');
+    message.reply(`Pong! \'${Date.now() - message.createdTimestamp} ms \``);
   }
+
+  // ask who the bot is
   if (command == 's' && args[0] == 'だれですか') {
     message.reply('なのです');
   }
 
+  /*
+  Takes the last syllable of the argument and looks for a new word
+  that starts with that syllable.
+  e.g. かぞく　--> くに
+  */
   if (command == 's' && args[0] != 'だれですか') {
     // check if the argument is written in hiragana/katakana
     var regex = /[\u3000-\u303F]|[\u3040-\u309F]|[\u30A0-\u30FF]/
@@ -37,42 +45,43 @@ client.on('message', message => {
       message.channel.send('かなを書いてください');
       return;
     }
-    //console.log(syllable);
+
+    // encodes special characters (any japanese character) for the query
     var query = encodeURI(syllable);
 
-    var res;
     request('http://jisho.org/api/v1/search/words?keyword=' + query + '*', function (error, response, body) {
       console.log('error:', error); // Print the error if one occurred
       console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 
-      //console.log(body);
-      res = JSON.parse(body);
-      console.log(res.data.length > 0);
+      // parses the data received in string format
+      var res = JSON.parse(body);
+
+      // if there is at least one result
       if (res.data.length > 0) {
-        //console.log(res.data);
+
+        // only deals with common words
         var data = (res.data).filter(res => res.is_common == true);
 
-        // pick a random word starting with the last syllable
+        // pick a random word in the array of results
         var randomIndex = Math.floor(Math.random() * data.length);
         var content = data[randomIndex];
-        // reroll if the reading is outdated (one-syllable words)
+
+        // reroll if the reading is outdated (especially for one-syllable words)
         while (! content.japanese[0].reading.startsWith(syllable) || content.japanese[0].reading.endsWith('ん')){
           randomIndex = Math.floor(Math.random() * data.length);
-          var content = data[randomIndex];
+          content = data[randomIndex];
         }
 
-        console.log(content);
-        //console.log(content.japanese);
-
-        // format the english definitions for the embed message
-        // make a list from the data
+        // formats the english definitions for the embed message
+        // makes a list from the data
         var senses = JSON.stringify(content.senses[0].english_definitions).replace(/\"/g, '').replace(/,/g, '\n');
-        // remove brackets
+        // removes brackets
         senses = senses.substring(1, senses.length-1);
         // markdown format
+        // adds a bullet at the beginning of each line
         senses = senses.replace(/^/gm, '・');
-        console.log(senses);
 
+        // message with the result to send
         message.channel.send({embed: {
           color: 3447003,
           author: {
@@ -80,56 +89,64 @@ client.on('message', message => {
             icon_url: client.user.avatarURL
           },
           fields: [{
+            // contains the word in kanji (or kana if there is no kanji)
             name: 'Word',
             value: content.japanese[0].word
           },
           {
+            // contains the reading in kana
             name: 'Reading',
             value: content.japanese[0].reading
           },
           {
+            // a list of english translations
             name: 'English definitions',
             value: senses
           }
           ]
         }});
       }
-      // no results
+      // if the query doesn't return any result
       else {
         message.channel.send('わかりません！')
       }
     });
   }
 
+  /*
+    Look up for the translation of a word.
+    Takes the first result of the query.
+  */
   if (command == 'def') {
-    let query = encodeURI(args[0]);
+    // encodes special characters (any japanese character) for the query
+    var query = encodeURI(syllable);
 
-    let res;
     request('http://jisho.org/api/v1/search/words?keyword=' + query + '*', function (error, response, body) {
       console.log('error:', error); // Print the error if one occurred
       console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
 
-      //console.log(body);
-      res = JSON.parse(body);
-      console.log(res.data.length > 0);
+      // parses the data received in string format
+      var res = JSON.parse(body);
+
+      // if there is at least one result
       if (res.data.length > 0) {
-        //console.log(res.data);
+
+        // only deals with common words
         var data = (res.data).filter(res => res.is_common == true);
 
-        //var randomIndex = Math.floor(Math.random() * data.length);
+        // takes the first result
         var content = data[0];
-        console.log(content);
-        //console.log(content.japanese);
 
-        // format the english definitions for the embed message
-        // make a list from the data
+        // formats the english definitions for the embed message
+        // makes a list from the data
         var senses = JSON.stringify(content.senses[0].english_definitions).replace(/\"/g, '').replace(/,/g, '\n');
-        // remove brackets
+        // removes brackets
         senses = senses.substring(1, senses.length-1);
         // markdown format
+        // adds a bullet at the beginning of each line
         senses = senses.replace(/^/gm, '・');
-        console.log(senses);
 
+        // message with the result to send
         message.channel.send({embed: {
           color: 3447003,
           author: {
@@ -137,21 +154,24 @@ client.on('message', message => {
             icon_url: client.user.avatarURL
           },
           fields: [{
+            // contains the word in kanji (or kana if there is no kanji)
             name: 'Word',
             value: content.japanese[0].word
           },
           {
+            // contains the reading in kana
             name: 'Reading',
             value: content.japanese[0].reading
           },
           {
+            // a list of english translations
             name: 'English definitions',
-            value: senses,
+            value: senses
           }
           ]
         }});
       }
-      // no results
+      // if the query doesn't return any result
       else {
         message.channel.send('わかりません！')
       }
@@ -159,4 +179,5 @@ client.on('message', message => {
   }
 });
 
+// connect to the server
 client.login(settings.token);
